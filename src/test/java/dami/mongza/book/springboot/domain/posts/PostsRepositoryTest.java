c41e5638a1b7b00b2a7fc04d497e541e39cd75ed
Dami <fdfdsa123@hanmail.net>
2020-05-06 21:43:07 +0900

    게시판 기능 추가 (p.144)
    - 머스테치 템플릿 엔진 추가
    - 메인, 글 등록 화면 추가 및 URL 매핑
    - 머스테치 레이아웃 추가 (header, footer)
    - 글 등록 API 호출해 글 등록하는 기능 추가
    - js 추가
    - JPA Auditing 추가 (LocalDateTime 활용)
    - 추가한 기능들 테스트코드 추가

--- src/test/java/dami/mongza/book/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/dami/mongza/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
@@ -38,5 +39,24 @@ public class PostsRepositoryTest {
 		Posts posts = postsList.get(0);
 		Assertions.assertThat(posts.getTitle()).isEqualTo(title);
 		Assertions.assertThat(posts.getContent()).isEqualTo(content);
+	}
+
+	@Test
+	public void testSetBaseTimeEntity() {
+		LocalDateTime now = LocalDateTime.of(2020, 5, 6, 0, 0, 0);
+		postsRepository.save(Posts.builder()
+				.title("title")
+				.content("content")
+				.author("author")
+				.build());
+
+		List<Posts> postsList = postsRepository.findAll();
+
+		Posts posts = postsList.get(0);
+
+		System.out.println(">>>>>>>>>>> CreateDate="+posts.getCreatedDate()+", ModifiedDate="+posts.getModifiedDate());
+
+		Assertions.assertThat(posts.getCreatedDate()).isAfter(now);
+		Assertions.assertThat(posts.getModifiedDate()).isAfter(now);
 	}
 }
