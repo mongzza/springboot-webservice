10a15e0dd6e36ef4423ae1bef74707f33db5f232
Dami <fdfdsa123@hanmail.net>
2020-06-16 23:23:51 +0900

    기능 추가
    - 게시글 수정, 삭제 기능 추가
    - 메인페이지에서 전체 게시글 조회 기능 추가
    - 부트스트랩 js 버전 업그레이드

--- src/main/java/dami/mongza/book/springboot/service/posts/PostsService.java
+++ src/main/java/dami/mongza/book/springboot/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package dami.mongza.book.springboot.service.posts;
 
 import dami.mongza.book.springboot.domain.posts.Posts;
 import dami.mongza.book.springboot.domain.posts.PostsRepository;
+import dami.mongza.book.springboot.web.dto.PostsListResponseDto;
 import dami.mongza.book.springboot.web.dto.PostsResponseDto;
 import dami.mongza.book.springboot.web.dto.PostsSaveRequestDto;
 import dami.mongza.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -33,5 +36,23 @@ public class PostsService {
 	public PostsResponseDto findById(Long id) {
 		Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
 		return new PostsResponseDto(entity);
+	}
+
+	/**
+	 * postsRepository.findAllDesc()로 받아온 Posts의 Stream을 map을 통해 PostsListResponseDto로 변환 후 List로 반환하는 메소드
+	 * @return 전체 posts 목록
+	 */
+	@Transactional(readOnly = true)
+	public List<PostsListResponseDto> findAllDesc() {
+		return postsRepository.findAllDesc().stream()
+				.map(PostsListResponseDto::new)  // == .map(posts -> new PostsListResponseDto(posts))
+				.collect(Collectors.toList());
+	}
+
+	@Transactional
+	public void delete(Long id) {
+		Posts posts = postsRepository.findById(id)
+				.orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+		postsRepository.delete(posts);
 	}
 }
